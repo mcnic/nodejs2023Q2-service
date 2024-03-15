fb245edd4d7606d435b2e6c6222aa8e4928e3924
max <mcnic@users.noreply.github.com>
2024-03-14 22:18:25 -0700

    feat: move Users to  prisma

--- src/components/users/user.module.ts
+++ src/components/users/user.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { MemoryDbProvider } from 'src/db/memoryStore';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
   controllers: [UserController],
   providers: [UserService, MemoryDbProvider],
+  imports: [PrismaModule],
 })
 export class UserModule {}
--- src/components/users/user.service.ts
+++ src/components/users/user.service.ts
@@ -1,56 +1,52 @@
 import {
   ForbiddenException,
-  Inject,
   Injectable,
   NotFoundException,
 } from '@nestjs/common';
 import { ShowingUser, User } from '../user.interface';
 import { newUUID } from 'src/helpers/uuid';
-import { getTimestamp } from 'src/helpers/time';
-import { MEMORY_STORE } from 'src/db/memoryStore';
-import { MemoryStore } from 'src/db/memoryStore';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class UserService {
-  constructor(@Inject(MEMORY_STORE) private readonly store: MemoryStore) {}
+  constructor(private prisma: PrismaService) {}
 
   async assertUserExistById(id: string) {
-    const user = await this.isExist(id);
+    const user = await this.prisma.user.findFirst({
+      where: { id },
+    });
 
     if (!user) throw new NotFoundException('User not found');
   }
 
   getShowngUser(user: User): ShowingUser {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const { password: _password, ...userWithoutPassword } = user;
+    const { id, login, version, createdAt, updatedAt } = user;
 
-    return { ...userWithoutPassword };
+    return {
+      id,
+      login,
+      version,
+      createdAt: createdAt.getTime(),
+      updatedAt: updatedAt.getTime(),
+    };
   }
 
   async getAll(): Promise<User[]> {
-    const store = await this.store.getStore();
-
-    return store.users;
+    return await this.prisma.user.findMany();
   }
 
   async isExist(userId: string): Promise<boolean> {
-    const store = await this.store.getStore();
+    const user = await this.prisma.user.findFirst({
+      where: { id: userId },
+    });
 
-    return !!store.users.find(({ id }) => id === userId);
-  }
-
-  async isPasswordCorrect(
-    userId: string,
-    userPassword: string,
-  ): Promise<boolean> {
-    const user = await this.getById(userId);
-
-    return user.password === userPassword;
+    return !!user;
   }
 
   async getById(userId: string): Promise<User | undefined> {
-    const store = await this.store.getStore();
-    const user = store.users.find(({ id }) => id === userId);
+    const user = await this.prisma.user.findFirst({
+      where: { id: userId },
+    });
 
     if (!user) throw new NotFoundException('User not found');
 
@@ -58,30 +54,30 @@ export class UserService {
   }
 
   async getByLogin(userLogin: string): Promise<User> {
-    const store = await this.store.getStore();
-
-    return store.users.find(({ login }) => login === userLogin);
+    return await this.prisma.user.findFirst({
+      where: { login: userLogin },
+    });
   }
 
   async add(login: string, password: string): Promise<User> {
-    const user = await this.getByLogin(login);
+    const user = await this.prisma.user.findFirst({
+      where: { login },
+    });
 
     if (user) return user;
 
-    const timestamp = getTimestamp();
-    const newUser: User = {
-      id: newUUID(),
-      login,
-      password,
-      version: 1,
-      createdAt: timestamp,
-      updatedAt: timestamp,
-    };
+    const timestamp = new Date();
 
-    const store = await this.store.getStore();
-    const users = store.users;
-    users.push(newUser);
-    await this.store.setStore({ ...store, users });
+    const newUser = await this.prisma.user.create({
+      data: {
+        id: newUUID(),
+        login,
+        password,
+        version: 1,
+        createdAt: timestamp,
+        updatedAt: timestamp,
+      },
+    });
 
     return newUser;
   }
@@ -91,28 +87,27 @@ export class UserService {
     oldPassword: string,
     newPassword: string,
   ) {
-    if (!(await this.isPasswordCorrect(id, oldPassword)))
-      throw new ForbiddenException('OldPassword is wrong');
+    const user = await this.getById(id);
 
-    const store = await this.store.getStore();
-    const users = store.users.map((user) => {
-      return user.id === id
-        ? {
-            ...user,
-            password: newPassword,
-            updatedAt: getTimestamp(),
-            version: user.version + 1,
-          }
-        : user;
+    if (user.password !== oldPassword) {
+      throw new ForbiddenException('OldPassword is wrong');
+    }
+
+    await this.prisma.user.update({
+      where: { id },
+      data: {
+        password: newPassword,
+        version: user.version + 1,
+        updatedAt: new Date(),
+      },
     });
-    await this.store.setStore({ ...store, users });
   }
 
-  async removedById(userId: string) {
-    await this.assertUserExistById(userId);
+  async removedById(id: string) {
+    await this.assertUserExistById(id);
 
-    const store = await this.store.getStore();
-    const users = store.users.filter(({ id }) => id !== userId);
-    await this.store.setStore({ ...store, users });
+    await this.prisma.user.delete({
+      where: { id },
+    });
   }
 }
